4319752c609d92dff0d5717e341e240a75cec260
snigdhagupta <snigi.gupta@gmail.com>
2020-10-25 01:03:59 -0400

    Modified Leetcode #26, added better approach

--- Remove_Duplicates_from_Sorted_Array.py
+++ Remove_Duplicates_from_Sorted_Array.py
@@ -16,6 +16,23 @@ class Solution:
         return i+1, nums[:i+1]
 
 
+    """
+    A better approach is to only consider when nums[i] != nums[j]
+    and simply perform requried actions
+    This approach has higher code readability
+    """
+
+    def betterApproach(self, nums):
+        i, j = 0, 1
+        while j < len(nums):
+            if nums[i] != nums[j]:
+                nums[i+1] = nums[j]
+                i += 1
+            j +=1
+        return i+1, nums[:i+1]
+
+
 if __name__ == "__main__":
     obj = Solution()
     print(obj.removeDuplicates(nums=[1,1,1,2,2,3,4]))
+    print(obj.betterApproach(nums=[1,1,1,2,2,3,4]))
